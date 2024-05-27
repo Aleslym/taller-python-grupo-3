b8d332838ca9c3f004847c2a4e938aa9e0906d3a
Aleslym <Alejandrad2710@gmail.com>
2024-05-27 14:28:49 -0500

    4. SERIE DE TERMINOSu.py

--- 4. SERIE DE TERMINOS.py
+++ 4. SERIE DE TERMINOS.py
@@ -1,7 +1,5 @@
 n=20
 suma=0
-#la serie inicia en 1 hasta completar 20 numeros que se van sumando 1 al numero anterior
-#mientras se guarda en i
 for i in range(1,n+1):
     suma +=i
     #lo que hay en la variable suma, se le agrega el proceso que ha hecho i
