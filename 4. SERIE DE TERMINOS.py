93c13b277062f72f260ff7731d1e1a4a4ee39d10
Aleslym <Alejandrad2710@gmail.com>
2024-05-28 05:39:11 -0500

    4. SERIE DE TERMINOS.py

--- 4. SERIE DE TERMINOS.py
+++ 4. SERIE DE TERMINOS.py
@@ -1,7 +1,6 @@
 n=20
 suma=0
-for i in range(1,n+1):
-    suma +=i
-    #lo que hay en la variable suma, se le agrega el proceso que ha hecho i
-    print(suma)
+for aumento in range (1,21):
+  suma=suma+aumento
+print (suma)
  
